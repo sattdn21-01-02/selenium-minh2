c3f2cc35f82dd50fc0a84b4076eb683936d87f3a
SATTDN21.01.02 <tnminh1997123@gmail.com>
2021-05-04 10:22:24 +0700

    start new project Railway 3

--- src/main/java/helper/Constant.java
+++ src/main/java/helper/Constant.java
@@ -7,7 +7,7 @@ public class Constant {
     public static final int TIME_WAITS_DRIVER = 30;
     public static final int TIMES_WAIT_ELEMENTS = 120;
     public static final int LONG_TIME_WAIT = 30;
-
+    public static final String DATE_FORMAT = "M/d/yyyy";
 
 
     //Message
