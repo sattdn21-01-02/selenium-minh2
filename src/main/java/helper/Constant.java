ba705b30ac596e4ec127688ebd458e4084e01bd7
SATTDN21.01.02 <tnminh1997123@gmail.com>
2021-05-04 14:49:04 +0700

    complete TC03

--- src/main/java/helper/Constant.java
+++ src/main/java/helper/Constant.java
@@ -3,7 +3,9 @@ package helper;
 public class Constant {
 
     //Variables
+    public static final String RAILWAY_URL = "http://www.railway2.somee.com/Page/HomePage.cshtml";
     public static final String TEST_RESOURCES_PATH = "/src/test/resources/";
+    public static final String USERNAME = "tnminh1997123@gmail.com";
     public static final int TIME_WAITS_DRIVER = 30;
     public static final int TIMES_WAIT_ELEMENTS = 120;
     public static final int LONG_TIME_WAIT = 30;
@@ -11,5 +13,5 @@ public class Constant {
 
 
     //Message
-
+    public static final String FAIL_MSG_LOGIN_INVALID_ACCOUNT = "Invalid username or password. Please try again.";
 }
---
+++ src/test/java/com/logigear/LoginTest.java
@@ -0,0 +1,35 @@
+package com.logigear;
+
+import com.sun.org.glassfish.gmbal.Description;
+import helper.Constant;
+import helper.DataHelper;
+import helper.LoggerHelper;
+import models.Account;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import page_objects.HomePage;
+import page_objects.LoginPage;
+
+public class LoginTest extends BaseTest{
+
+    private final HomePage homePage = new HomePage();
+    private final LoginPage loginPage = new LoginPage();
+
+    @Description("TC03 - User cannot log into Railway with invalid password ")
+    @Test
+    public void TC03() {
+        LoggerHelper.startTestCase("TC03 - User cannot log into Railway with invalid password ");
+
+        LoggerHelper.info("[STEP-1] - Click on login tab");
+        homePage.goToLoginPage();
+
+        LoggerHelper.info("[STEP-2] - Login unsuccessful with valid email and invalid password");
+        Account account = new Account(Constant.USERNAME, DataHelper.getRandomErrorPassword());
+        loginPage.login(account);
+
+        LoggerHelper.info("[STEP-3] - Assert error message is displays");
+        String actualMsg = loginPage.getGeneralErrorMessage();
+        String expectedMsg = Constant.FAIL_MSG_LOGIN_INVALID_ACCOUNT;
+        Assert.assertEquals(actualMsg, expectedMsg, actualMsg+" is not matched with "+expectedMsg);
+    }
+}
