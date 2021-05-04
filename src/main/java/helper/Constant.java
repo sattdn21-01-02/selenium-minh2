09e0260600d1af8e8c86b4e2ce2b52fb35100d35
SATTDN21.01.02 <tnminh1997123@gmail.com>
2021-05-04 13:59:24 +0700

    Complete TC02

--- src/main/java/helper/Constant.java
+++ src/main/java/helper/Constant.java
@@ -3,13 +3,15 @@ package helper;
 public class Constant {
 
     //Variables
+    public static final String RAILWAY_URL = "http://www.railway2.somee.com/Page/HomePage.cshtml";
     public static final String TEST_RESOURCES_PATH = "/src/test/resources/";
     public static final int TIME_WAITS_DRIVER = 30;
     public static final int TIMES_WAIT_ELEMENTS = 120;
     public static final int LONG_TIME_WAIT = 30;
     public static final String DATE_FORMAT = "M/d/yyyy";
-
+    public static final String BLANK_EMAIL = "";
+    public static final String PASSWORD = "minhvip1997";
 
     //Message
-
+    public static final String FAIL_MSG_REGISTER = "There was a problem with your login and/or errors exist in your form.";
 }
---
+++ src/test/java/com/logigear/LoginTest.java
@@ -0,0 +1,34 @@
+package com.logigear;
+
+import com.sun.org.glassfish.gmbal.Description;
+import helper.Constant;
+import helper.LoggerHelper;
+import models.Account;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import page_objects.HomePage;
+import page_objects.LoginPage;
+
+public class LoginTest extends BaseTest {
+
+    private final HomePage homePage = new HomePage();
+    private final LoginPage loginPage = new LoginPage();
+
+    @Description("TC02 - User can't login with blank Username textbox")
+    @Test
+    public void TC02() {
+        LoggerHelper.startTestCase("TC02 - User can't login with blank Username textbox");
+
+        LoggerHelper.info("[STEP-1] - Click on login tab");
+        homePage.goToLoginPage();
+
+        LoggerHelper.info("[STEP-2] - Login unsuccessful with blank email and valid password");
+        Account account = new Account(Constant.BLANK_EMAIL, Constant.PASSWORD);
+        loginPage.login(account);
+
+        LoggerHelper.info("[STEP-3] - Assert error message is displays");
+        String actualMsg = loginPage.getGeneralErrorMessage();
+        String expectedMsg = Constant.FAIL_MSG_REGISTER;
+        Assert.assertEquals(actualMsg, expectedMsg, actualMsg + "is not matched with" + expectedMsg);
+    }
+}
