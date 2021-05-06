75e6a7a3104a8362fa3a38a628d651db79bcebb6
SATTDN21.01.02 <tnminh1997123@gmail.com>
2021-05-06 15:12:13 +0700

    áºedit áºerror mÃesage

--- src/test/java/com/logigear/RegisterTest.java
+++ src/test/java/com/logigear/RegisterTest.java
@@ -32,7 +32,7 @@ public class RegisterTest extends BaseTest {
         LoggerHelper.startTestCase("TC11 - User can't create account while password and PID fields are empty");
 
         String expectedErrorMsg = "There're errors in the form. Please correct the errors and try again.";
-        String expectedErrorPasswordMsg = "Invalid password length";
+        String expectedPasswordErrorMsg = "Invalid password length";
         String expectedErrorPidMsg = "Invalid ID length";
 
         homePage.goToRegisterPage();
@@ -40,11 +40,11 @@ public class RegisterTest extends BaseTest {
         registerPage.register(account);
 
         String actualErrorMsg = registerPage.getGeneralErrorMessage();
-        String actualErrorPasswordMsg = registerPage.getPasswordErrorMessage();
-        String actualErrorPidMsg = registerPage.getPidErrorMessage();
+        String actualPasswordErrorMsg = registerPage.getPasswordErrorMessage();
+        String actualPidErrorMsg = registerPage.getPidErrorMessage();
 
         Assert.assertEquals(actualErrorMsg, expectedErrorMsg, "Error message fails to display! ");
-        Assert.assertEquals(actualErrorPasswordMsg, expectedErrorPasswordMsg, "Error password message fails to display!");
-        Assert.assertEquals(actualErrorPidMsg, expectedErrorPidMsg, "Error pid message fails to display!");
+        Assert.assertEquals(actualPasswordErrorMsg, expectedPasswordErrorMsg, "Error password message fails to display!");
+        Assert.assertEquals(actualPidErrorMsg, expectedErrorPidMsg, "Error pid message fails to display!");
     }
 }
