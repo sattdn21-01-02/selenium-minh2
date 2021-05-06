04caaba4c27a3dfaa33cf8010fb4641d7ead3405
SATTDN21.01.02 <tnminh1997123@gmail.com>
2021-05-06 14:57:37 +0700

    create page_objects TC16

--- src/main/java/page_objects/MyTicketPage.java
+++ src/main/java/page_objects/MyTicketPage.java
@@ -1,5 +1,8 @@
 package page_objects;
 
+import helper.BrowserHelper;
+import helper.ElementHelper;
+import helper.element_helper.Button;
 import helper.element_helper.Label;
 import org.openqa.selenium.By;
 
@@ -7,9 +10,19 @@ public class MyTicketPage extends GeneralPage {
 
     //Elements
     private final Label lblMyTicket = new Label(By.cssSelector("#content h1"));
+    Button btnCancel = new Button(By.cssSelector("[value=Cancel]"));
+    String xPath = "//td[count(//th[text()='No.']) and text() = %d]//following-sibling::td//input";
 
     //Methods
     public String getMyTicketTitle() {
         return this.lblMyTicket.getText();
+    }
+
+    public void cancelSpecifiedTicket(int id) {
+        String xPaths = String.format(xPath, id);
+        Button btnCancel = new Button(By.xpath(xPaths));
+        ElementHelper.scrollToView(btnCancel.findElement());
+        this.btnCancel.click();
+        BrowserHelper.acceptAlert();
     }
 }
